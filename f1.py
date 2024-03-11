ed88794d94a9bea86c0ff29cce8ce60fd4259b69
Ranga19 <gaddamranganath32@gmail.com>
2024-03-12 01:20:37 +0530

    final changes3

--- f1.py
+++ f1.py
@@ -1,49 +1,44 @@
 from django.db import models
-from rest_framework import serializers, viewsets, routers
-from django.urls import path, include
+from rest_framework import serializers, viewsets
 
 class Author(models.Model):
-name = models.CharField(max_length=100)
-birth_date = models.DateField()
+    """
+    a
+    """
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
 
 class Book(models.Model):
-title = models.CharField(max_length=100)
-author = models.ForeignKey(Author, on_delete=models.CASCADE)
-publication_date = models.DateField()
+    """b"""
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField()
 
 class AuthorSerializer(serializers.ModelSerializer):
-class Meta:
-model = Author
-fields = '__all__'
+    """c"""
+    class Meta:
+        model = Author
+        fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
-class Meta:
-model = Book
-fields = '__all__'
+    """c"""
+    class Meta:
+        model = Book
+        fields = '__all__'
 
 class AuthorViewSet(viewsets.ModelViewSet):
-queryset = Author.objects.all()
-serializer_class = AuthorSerializer
+    """d"""
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
 
-def get_queryset(self):
-return Author.objects.filter(name__icontains=self.request.query_params.get('name', ''))
+    def get_queryset(self):
+        return Author.objects.filter(name__icontains=self.request.query_params.get('name', ''))
 
 class BookViewSet(viewsets.ModelViewSet):
-queryset = Book.objects.all()
-serializer_class = BookSerializer
+    """e"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-def get_queryset(self):
-return Book.objects.filter(author_id=self.request.query_params.get('author_id'))
-
-router = routers.DefaultRouter()
-router.register('authors', AuthorViewSet)
-router.register('books', BookViewSet)
-
-urlpatterns = [
-path('api/', include(router.urls)),
-]
-
-# In your project's urls.py
-urlpatterns += [
-path('api/', include('myapp.urls')),
-]
+    def get_queryset(self):
+        return Book.objects.filter(author_id=self.request.query_params.get('author_id'))
+    
