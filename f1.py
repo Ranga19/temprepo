30753537fb9fa8b4e8e85ce72a79c5beed080afc
Ranga19 <gaddamranganath32@gmail.com>
2024-03-12 00:57:26 +0530

    f1 changes

--- f1.py
+++ f1.py
@@ -2,59 +2,48 @@ from django.db import models
 from rest_framework import serializers, viewsets, routers
 from django.urls import path, include
 
-
 class Author(models.Model):
-
-
-name = models.CharField(max_length=100) 
+name = models.CharField(max_length=100)
 birth_date = models.DateField()
 
-
 class Book(models.Model):
-
-
 title = models.CharField(max_length=100)
 author = models.ForeignKey(Author, on_delete=models.CASCADE)
 publication_date = models.DateField()
 
-
 class AuthorSerializer(serializers.ModelSerializer):
 class Meta:
-
-
 model = Author
 fields = '__all__'
 
-
 class BookSerializer(serializers.ModelSerializer):
 class Meta:
-
-
 model = Book
 fields = '__all__'
 
-
 class AuthorViewSet(viewsets.ModelViewSet):
-
-
 queryset = Author.objects.all()
 serializer_class = AuthorSerializer
 
+def get_queryset(self):
+return Author.objects.filter(name__icontains=self.request.query_params.get('name', ''))
+
+class BookViewSet(viewsets.ModelViewSet):
+queryset = Book.objects.all()
+serializer_class = BookSerializer
 
 def get_queryset(self):
+return Book.objects.filter(author_id=self.request.query_params.get('author_id'))
 
+router = routers.DefaultRouter()
+router.register('authors', AuthorViewSet)
+router.register('books', BookViewSet)
 
-return Author.objects.filter(name__icontains=self.request.query_params.get('name', '')) 
+urlpatterns = [
+path('api/', include(router.urls)),
+]
 
-
-class BookViewSet(viewsets.ModelViewSet): 
-
-
-queryset = Book.objects.all() 
-serializer_class = BookSerializer 
-
-
-def get_queryset(self):
-
-
-return Book.objects.filter(author_id=self.request.query_params.get('author_id')) 
+# In your project's urls.py
+urlpatterns += [
+path('api/', include('myapp.urls')),
+]
